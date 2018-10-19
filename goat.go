f7de0431dbbc29a5f62c62804aeef8f847ecf35f
Tomas Jasek <tomsik68@gmail.com>
2018-10-19 20:09:34 +0200

    Add -out-dir and -templates-dir arguments
    -out-dir is output directory for processed accounting data
    -templates-dir is directory for template files used during processing
    accounting data

--- goat.go
+++ goat.go
@@ -9,11 +9,13 @@ import (
 
 // CLI option names
 var (
-	ip       = flag.String("listen-ip", "127.0.0.1", "IP address to bind to")
-	port     = flag.Uint("port", 9623, "port to bind to")
-	tls      = flag.Bool("tls", false, "True uses TLS, false uses plaintext TCP")
-	certFile = flag.String("cert-file", "server.pem", "server certificate file")
-	keyFile  = flag.String("key-file", "server.key", "server key file")
+	ip           = flag.String("listen-ip", "127.0.0.1", "IP address to bind to")
+	port         = flag.Uint("port", 9623, "port to bind to")
+	tls          = flag.Bool("tls", false, "True uses TLS, false uses plaintext TCP")
+	certFile     = flag.String("cert-file", "server.pem", "server certificate file")
+	keyFile      = flag.String("key-file", "server.key", "server key file")
+	outDir       = flag.String("out-dir", "", "output directory")
+	templatesDir = flag.String("templates-dir", "", "templates directory")
 )
 
 func checkArgs() error {
@@ -25,6 +27,15 @@ func checkArgs() error {
 			return fmt.Errorf("Please specify a -key-file")
 		}
 	}
+
+	if *outDir == "" {
+		return fmt.Errorf("Please specify an -out-dir")
+	}
+
+	if *templatesDir == "" {
+		return fmt.Errorf("Please specify a -templates-dir")
+	}
+
 	return nil
 }
 
