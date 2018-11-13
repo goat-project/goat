784acd29334266a3d565fa4505b45d0dba5677b6
Tomas Jasek <tomsik68@gmail.com>
2018-11-13 14:50:44 +0100

    Use ResultsChannel in Writer skeleton

--- consumer/writer.go
+++ consumer/writer.go
@@ -20,19 +20,19 @@ func NewWriter(dir, templatesDir string) WriterConsumer {
 }
 
 // ConsumeIps transforms IpRecord-s into text and writes them to a subdirectory of dir specified by WriterConsumer's dir field. Each IpRecord is written to its own file.
-func (wc WriterConsumer) ConsumeIps(ctx context.Context, id string, ips <-chan goat_grpc.IpRecord) (DoneChannel, error) {
-	done := make(chan struct{})
-	return done, nil
+func (wc WriterConsumer) ConsumeIps(ctx context.Context, id string, ips <-chan goat_grpc.IpRecord) (ResultsChannel, error) {
+	res := make(chan ConsumerResult)
+	return res, nil
 }
 
 // ConsumeVms transforms VmRecord-s into text and writes them to a subdirectory of dir specified by WriterConsumer's dir field. Each VmRecord is written to its own file.
-func (wc WriterConsumer) ConsumeVms(ctx context.Context, id string, vms <-chan goat_grpc.VmRecord) (DoneChannel, error) {
-	done := make(chan struct{})
-	return done, nil
+func (wc WriterConsumer) ConsumeVms(ctx context.Context, id string, vms <-chan goat_grpc.VmRecord) (ResultsChannel, error) {
+	res := make(chan ConsumerResult)
+	return res, nil
 }
 
 // ConsumeStorages transforms StorageRecord-s into text and writes them to a subdirectory of dir specified by WriterConsumer's dir field. Each StorageRecord is written to its own file.
-func (wc WriterConsumer) ConsumeStorages(ctx context.Context, id string, sts <-chan goat_grpc.StorageRecord) (DoneChannel, error) {
-	done := make(chan struct{})
-	return done, nil
+func (wc WriterConsumer) ConsumeStorages(ctx context.Context, id string, sts <-chan goat_grpc.StorageRecord) (ResultsChannel, error) {
+	res := make(chan ConsumerResult)
+	return res, nil
 }
