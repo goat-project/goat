1c2789992375d40f09cbbc59dee043cb7f276d80
Tomas Jasek <tomsik68@gmail.com>
2018-11-20 13:47:08 +0100

    Rename ConsumerResult to Result

--- consumer/results_channel.go
+++ consumer/results_channel.go
@@ -4,39 +4,39 @@ import (
 	"sync"
 )
 
-// ConsumerResult encapsulates a result of consume method. It is either erroneous result or successful
-type ConsumerResult struct {
+// Result encapsulates a result of consume method. It is either erroneous result or successful
+type Result struct {
 	ok  bool
 	err error
 }
 
-// ResultsChannel is a read-only channel of ConsumerResults
-type ResultsChannel <-chan ConsumerResult
+// ResultsChannel is a read-only channel of Results
+type ResultsChannel <-chan Result
 
 // ErrHandler is a function that handles specified error
 type ErrHandler func(error)
 
 // Error returns the underlying error in case this result is successful. Otherwise returns nil
-func (cr ConsumerResult) Error() error {
+func (cr Result) Error() error {
 	return cr.err
 }
 
 // IsOK returns true if and only if this result is successful
-func (cr ConsumerResult) IsOK() bool {
+func (cr Result) IsOK() bool {
 	return cr.ok
 }
 
 // NewErrorResult creates a new unsuccessful result with specified underlying error
-func NewErrorResult(err error) ConsumerResult {
-	return ConsumerResult{
+func NewErrorResult(err error) Result {
+	return Result{
 		err: err,
 		ok:  false,
 	}
 }
 
 // NewOkResult creates a new successful result
-func NewOkResult() ConsumerResult {
-	return ConsumerResult{
+func NewOkResult() Result {
+	return Result{
 		err: nil,
 		ok:  true,
 	}
