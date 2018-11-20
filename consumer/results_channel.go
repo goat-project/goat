83878b08716bad698098ab5f2e6a2a6706c4e70f
Tomas Jasek <tomsik68@gmail.com>
2018-11-20 13:37:58 +0100

    Add documentation comments

--- consumer/results_channel.go
+++ consumer/results_channel.go
@@ -4,22 +4,29 @@ import (
 	"sync"
 )
 
+// ConsumerResult encapsulates a result of consume method. It is either erroneous result or successful
 type ConsumerResult struct {
 	ok  bool
 	err error
 }
 
+// ResultsChannel is a read-only channel of ConsumerResults
 type ResultsChannel <-chan ConsumerResult
+
+// ErrHandler is a function that handles specified error
 type ErrHandler func(error)
 
+// Error returns the underlying error in case this result is successful. Otherwise returns nil
 func (cr ConsumerResult) Error() error {
 	return cr.err
 }
 
+// IsOK returns true if and only if this result is successful
 func (cr ConsumerResult) IsOK() bool {
 	return cr.ok
 }
 
+// NewErrorResult creates a new unsuccessful result with specified underlying error
 func NewErrorResult(err error) ConsumerResult {
 	return ConsumerResult{
 		err: err,
@@ -27,6 +34,7 @@ func NewErrorResult(err error) ConsumerResult {
 	}
 }
 
+// NewOkResult creates a new successful result
 func NewOkResult() ConsumerResult {
 	return ConsumerResult{
 		err: nil,
@@ -34,6 +42,7 @@ func NewOkResult() ConsumerResult {
 	}
 }
 
+// CheckResults reads all results from given results channels. When an error is encountered, onError is called with the underlying error
 func CheckResults(onError ErrHandler, chans ...ResultsChannel) {
 	var wg sync.WaitGroup
 	multiplex := func(c ResultsChannel) {
