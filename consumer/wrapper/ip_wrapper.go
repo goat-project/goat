5e6e963aaa37526844fcb678857bfefbf5abd527
Lenka <lenocka369@gmail.com>
2019-07-04 10:49:22 +0200

    Add attributes to ipJSON struct

--- consumer/wrapper/ip_wrapper.go
+++ consumer/wrapper/ip_wrapper.go
@@ -9,7 +9,16 @@ type ipWrapper struct {
 }
 
 type ipJSON struct {
-	// TODO
+	MeasurementTime     int64
+	SiteName            string
+	CloudComputeService *string
+	CloudType           string
+	LocalUser           string
+	LocalGroup          string
+	GlobalUserName      string
+	FQAN                string
+	IPVersion           byte
+	IPCount             int
 }
 
 // NewIPWrapper wraps given ip in a RecordWrapper
