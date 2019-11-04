2e3fd3ebd93c7266f23f9d926462a35ccaf1e5a0
Lenka <lenocka369@gmail.com>
2019-11-04 16:33:26 +0100

    Add logger

---
+++ logger/logger.go
@@ -0,0 +1,48 @@
+package logger
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Init initializes logrus by configuration.
+func Init(path *string, debug *bool) {
+	switch *path {
+	case "":
+		if *debug {
+			InitLogToStdoutDebug()
+		} else {
+			InitLogToStdout()
+		}
+	default:
+		InitLogToFile(*path)
+	}
+}
+
+// InitLogToStdoutDebug inits logrus to log the debug severity or above to Stdout.
+func InitLogToStdoutDebug() {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		ForceColors: true,
+	})
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.DebugLevel)
+}
+
+// InitLogToStdout inits logrus to log the info severity or above to Stdout.
+func InitLogToStdout() {
+	logrus.SetFormatter(&logrus.TextFormatter{})
+	logrus.SetOutput(os.Stdout)
+}
+
+// InitLogToFile inits logrus to log the info severity or above to the file.
+func InitLogToFile(logPath string) {
+	logrus.SetFormatter(&logrus.TextFormatter{})
+
+	f, err := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
+	if err != nil {
+		logrus.Fatalf("error opening file: %v", err)
+	}
+
+	logrus.SetOutput(f)
+}
