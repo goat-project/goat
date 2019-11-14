74fc90a93aee3f3f10a3ef2d37b8f330e1637213
Lenka <lenocka369@gmail.com>
2019-11-14 18:44:52 +0100

    Add consumer suite test

---
+++ consumer/consumer_suite_test.go
@@ -0,0 +1,13 @@
+package consumer
+
+import (
+	"testing"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+)
+
+func TestResources(t *testing.T) {
+	gomega.RegisterFailHandler(ginkgo.Fail)
+	ginkgo.RunSpecs(t, "Consumer Suite")
+}
